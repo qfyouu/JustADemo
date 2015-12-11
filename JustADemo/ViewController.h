541e2fbfb32e30e8ec1428a40bffc61856f00c6a
尤锐 <yourui@handsomeuu.local>
2015-12-11 11:28:41 +0800

    Initial Commit

---
+++ JustADemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JustADemo
+//
+//  Created by 尤锐 on 15/12/11.
+//  Copyright (c) 2015年 尤锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JustADemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JustADemo
+//
+//  Created by 尤锐 on 15/12/11.
+//  Copyright (c) 2015年 尤锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
